9af05a64fb9306f3f7dca67e5e7b6e7cc44ce057
Jaidlab <240663663+Jaidlab@users.noreply.github.com>
2026-03-12 12:28:40 +0100

    Fixed test
    Signed-off-by: Jaidlab <240663663+Jaidlab@users.noreply.github.com>

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,9 +1,9 @@
-import { it, expect } from "bun:test"
+import {expect, it} from 'bun:test'
 
 import {getMainModuleDefault} from 'zeug'
 
-const space = await getMainModuleDefault<typeof import('../src/index.ts')>('space')
-
-it("should run", () => {
-  expect(space).toBe(1) // TODO Test actual functionality
+const space = await getMainModuleDefault<typeof import('../src/main.ts')>('space')
+it('should run', () => {
+  expect(space).toBeFunction()
+  expect(space()).toBe('ඞ')
 })

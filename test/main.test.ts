294ce810177f5dba80ec9a973e5aece6088ffe01
Jaidlab <240663663+Jaidlab@users.noreply.github.com>
2026-03-13 05:18:50 +0100

    Fixed test
    Signed-off-by: Jaidlab <240663663+Jaidlab@users.noreply.github.com>

--- test/main.test.ts
+++ test/main.test.ts
@@ -2,7 +2,7 @@ import {expect, it} from 'bun:test'
 
 import {getMainModuleDefault} from 'zeug'
 
-const space = await getMainModuleDefault<typeof import('../src/main.ts')>('space')
+const space = await getMainModuleDefault<typeof import('../src/main.ts')>('src/main.ts')
 it('should run', () => {
   expect(space).toBeFunction()
   expect(space()).toBe('ඞ')
